cccfeeccc629e56d69d765ac80b8f330d6d4d64a
HadesYan <hedas1412@gmail.com>
2020-04-20 01:22:26 +0800

    modify godeps.json git log version

--- vendor/GoLearning/introduction/simpleServer.go
+++
@@ -1,8 +0,0 @@
-package introduction
-
-import "net/http"
-
-func Server() {
-	http.Handle("/", http.FileServer(http.Dir("."))) // 将当前目录作为根目录（/目录）的处理器，访问根目录，就会进入当前目录。
-	http.ListenAndServe(":1412", nil)
-}
